73c775473496a09749e32586dbd614bc2718ec05
Nikhil <33275592+Danton25@users.noreply.github.com>
2021-06-14 18:06:01 -0700

    Controller set up complete

--- src/main/java/org/example/controller/EmployeeController.java
+++ src/main/java/org/example/controller/EmployeeController.java
@@ -3,33 +3,37 @@ package org.example.controller;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
-import org.example.Employee;
+import org.example.entity.Employee;
 
 import java.util.ArrayList;
 import java.util.List;
 @RestController
-@Component
-//@RequestMapping(value = "/employees")
+@RequestMapping(value = "/employees")
 public class EmployeeController {
-    List<Employee> a = new ArrayList<>();
 
-    @RequestMapping(method = RequestMethod.GET, value = "/employees")
+
+    @RequestMapping(method = RequestMethod.GET)
     public List<Employee> findAll(){
-        return a;
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee("Nikhil", "nik@gmail.com", 100000));
+        employees.add(new Employee("Dan", "dan@gmail.com", 200000));
+        employees.add(new Employee("Gary", "gary@gmail.com", 300000));
+        return employees;
     }
-    @RequestMapping(method = RequestMethod.GET, value = "/employees/{id}")
+    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
     public Employee findOne(@PathVariable("id") String empId){
-        return null;
+        Employee emp = new Employee("Nikhil", "nik@gmail.com", 100000);
+        return emp;
     }
-//    @RequestMapping(method = RequestMethod.POST)
-//    public Employee create(@RequestBody Employee employee){
-//        return employee;
-//    }
-//    @RequestMapping(method = RequestMethod.PUT, value = "/{id}")
-//    public Employee update(@PathVariable("id") String empId, @RequestBody Employee employee) {
-//        return employee;
-//    }
-//    @RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
-//    public void delete(@PathVariable("id") String empId){
-//    }
+    @RequestMapping(method = RequestMethod.POST)
+    public Employee create(@RequestBody Employee employee){
+        return employee;
+    }
+    @RequestMapping(method = RequestMethod.PUT, value = "/{id}")
+    public Employee update(@PathVariable("id") String empId, @RequestBody Employee employee) {
+        return employee;
+    }
+    @RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
+    public void delete(@PathVariable("id") String empId){
+    }
 }
